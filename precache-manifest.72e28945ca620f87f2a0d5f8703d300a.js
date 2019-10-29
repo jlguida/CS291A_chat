6d9af57119aa694c81e3dbb114c83882d31dd77a
jake <jlguida@umail.ucsb.edu>
2019-10-29 10:08:39 -0700

    Updates

---
+++ precache-manifest.72e28945ca620f87f2a0d5f8703d300a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b79130b62075f0e79cae08730310c67",
+    "url": "/CS291A_chat/index.html"
+  },
+  {
+    "revision": "f701b881b15d43a8a088",
+    "url": "/CS291A_chat/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c0fd6905d5c3e5956bf3",
+    "url": "/CS291A_chat/static/css/main.00dd0acf.chunk.css"
+  },
+  {
+    "revision": "f701b881b15d43a8a088",
+    "url": "/CS291A_chat/static/js/2.3ea34a12.chunk.js"
+  },
+  {
+    "revision": "c0fd6905d5c3e5956bf3",
+    "url": "/CS291A_chat/static/js/main.7cfd22b0.chunk.js"
+  },
+  {
+    "revision": "926660780b03fbc224a9",
+    "url": "/CS291A_chat/static/js/runtime-main.7a2d22b9.js"
+  }
+]);
